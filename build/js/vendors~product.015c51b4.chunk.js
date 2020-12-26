6f471a5ffbcaf94a1ca88ce3c1548339ed98618a
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-12-26 14:20:20 +0200

    Initial build subtree commit

---
+++ build/js/vendors~product.015c51b4.chunk.js
@@ -0,0 +1,9 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["vendors~product"],{"./node_modules/@babel/runtime/helpers/get.js":
+/*!****************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/get.js ***!
+  \****************************************************/
+/*! no static exports found */function(e,r,t){var o=t(/*! ./superPropBase */"./node_modules/@babel/runtime/helpers/superPropBase.js");function n(r,t,s){return"undefined"!=typeof Reflect&&Reflect.get?e.exports=n=Reflect.get:e.exports=n=function(e,r,t){var n=o(e,r);if(n){var s=Object.getOwnPropertyDescriptor(n,r);return s.get?s.get.call(t):s.value}},n(r,t,s||r)}e.exports=n},"./node_modules/@babel/runtime/helpers/superPropBase.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/superPropBase.js ***!
+  \**************************************************************/
+/*! no static exports found */function(e,r,t){var o=t(/*! ./getPrototypeOf */"./node_modules/@babel/runtime/helpers/getPrototypeOf.js");e.exports=function(e,r){for(;!Object.prototype.hasOwnProperty.call(e,r)&&null!==(e=o(e)););return e}}}]);
---
+++ build/sw.js
@@ -0,0 +1,48 @@
+importScripts("./js/precache-manifest.c81cfa453734b27d337287d1ac47185e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+if (workbox) {
+  console.log('Yay! Workbox is loaded !')
+  workbox.precaching.precacheAndRoute([])
+
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => response || fetch(event.request))
+        .catch(() => caches.match('/'))
+    )
+  })
+
+  workbox.routing.registerRoute(
+    /(.*)others(.*)\.(?:png|gif|jpg|webp|jpeg)/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    })
+  )
+
+  workbox.routing.registerRoute(
+    /.*\.(?:css)/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'assets',
+    })
+  )
+  workbox.googleAnalytics.initialize()
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting()
+    }
+  })
+
+  workbox.core.clientsClaim()
+  workbox.precaching.precacheAndRoute(self.__precacheManifest)
+} else {
+  console.log("Oops! Workbox didn't load")
+}
+
